7f98a1f4419320b107140a60d3eaee33a8e8df19
jenkinhou <jenkinhou@gmail.com>
2019-01-05 18:03:30 +0800

    3.0

---
+++ Assets/Scripts/SunRotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunRotate : MonoBehaviour
+{
+    public int rotationSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+    }
+}
